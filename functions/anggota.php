398ef81d2fdf999556729e5af8d07e280077dbc6
N4thhh <natanaelbungaran@gmail.com>
2025-05-31 23:01:28 +0700

    add edit Anggota feature

--- functions/anggota.php
+++ functions/anggota.php
@@ -8,11 +8,11 @@ function getMembers(){
     return $result;
 }
 
-function getMember($member_id){
+function getMember($member_id) {
     global $conn;
     $sql = "SELECT * FROM anggota WHERE id = $member_id";
     $result = $conn->query($sql);
-    return $result;
+    return $result->fetch_assoc();
 }
 
 function addMember($name, $email, $password, $email_password, $email_password_confirm){
@@ -20,6 +20,22 @@ function addMember($name, $email, $password, $email_password, $email_password_co
     $sql = "INSERT INTO anggota (name, email, password) VALUES ('$name', '$email', '$password')";
     $result = $conn->query($sql);
     return $result;
+}
+
+function updateMember($id, $nama, $alamat, $nomor) {
+    global $conn;
+    $nama= mysqli_real_escape_string($conn, $nama);
+    $alamat= mysqli_real_escape_string($conn, $alamat);
+    $nomor= mysqli_real_escape_string($conn, $nomor);
+    $id= (int)$id;
+
+    $sql = "UPDATE anggota SET nama = '$nama', alamat = '$alamat', nomor = '$nomor' WHERE id = $id";
+
+    if ($conn->query($sql) === TRUE) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 ?>
