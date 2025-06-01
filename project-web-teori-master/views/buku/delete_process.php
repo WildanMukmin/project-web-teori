3aaa3b3161cffcb188d0af272f94359ebbf74c27
azmiihaniif <azmiihaniifah017@gmail.com>
2025-06-01 20:21:59 +0700

    Hapus folder project-web-teori

--- project-web-teori-master/views/buku/delete_process.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include_once '../../functions/buku.php';
-$id = $_GET['id'];
-deleteBook($id);
-header("Location: list.php");
-
-?>
--- project-web-teori-master/views/peminjaman/delete_process.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once '../../includes/db_connection.php';
-include_once '../../functions/peminjaman.php';
-$id = $_GET['id'];
-deleteTransactionById($id);
-header("Location: list.php");
-
-?>
